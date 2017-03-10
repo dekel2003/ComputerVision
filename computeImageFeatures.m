54a04582e3bf7c5d409e6254d819119cefe92a71
Dekel Matalon <dekel2003@gmail.com>
2017-03-10 16:35:20 +0200

    add funtion for computing given image features

---
+++ computeImageFeatures.m
@@ -0,0 +1,19 @@
+function [ featureList ] = computeImageFeatures( img )
+% Implementation for question1, section c.
+
+[Rows,Cols,~] = size(img);
+
+%% take LAB colors:
+labImg = rgb2lab(img);
+featureList = labImg;
+
+%% gabor filter:
+for colorChannel = 1:3
+    gaborArray = gaborFilterBank(5,8,39,39);
+    gaborFeatureVector = gaborFeatures(labImg(:,:,colorChannel),gaborArray,1 , 1);
+    features = reshape(gaborFeatureVector, Rows, Cols, []);
+    featureList = cat(3, featureList, features);
+end
+
+end
+
