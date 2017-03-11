79c61fff1ab0e343d82e56c8a7ff1038da468a72
itayz <itay.z@outlook.com>
2017-03-12 00:14:59 +0200

    small changes to Clustering.m

--- Clustering.m
+++ Clustering.m
@@ -1,13 +1,16 @@
-img = imread('LQ-20_dive5_2014-09-29.jpg');
-%features = ComputePositionColorFeatures(img);
-features = ComputeColorFeatures(img);
+img = imread('Eilat_REF_FLR_pairs/20_dive5_2014-09-29.jpg');
+img = imresize(img, 0.25);
+
+features = computeImageFeatures(img);
 featuresNorm = NormalizeFeatures(features);
-X = reshape(featuresNorm, [], size(featuresNorm, 3));
+X = reshape(featuresNorm, [], size(featuresNorm, 3));  
+
+[idx,c] = KMeansClustering(X,8);
 
 height = size(img, 1);
 width = size(img, 2);
 points = zeros(height, width, 2);
 [points(:,:,1), points(:,:,2)] = meshgrid(1:width, 1:height);
 points = reshape(points, [], 2);
-[idx,c] = KMeansClustering(X,20);
+
 VisualizeClusters2D(points, idx, c);
