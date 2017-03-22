fe8395e1aa6d580eab926dd65c3ede5c24efede5
itayz <itay.z@outlook.com>
2017-03-22 18:57:39 +0200

    add HAC example to run_me.m

--- run_me.m
+++ run_me.m
@@ -18,6 +18,24 @@ points = zeros(height, width, 2);
 points = reshape(points, [], 2);
 VisualizeClusters2D(points, idx, c, figure);
 
+%% q1 - clustering - HAC with 13 clusters, Position + Color features
+% The clustering script that tested all the cases is in Clustering.m.
+% This is just a small example.
+% img = imread('267_dive8_2014-09-29.jpg');
+% img = imresize(img, 0.04*2/3);
+% img = img(20:82,30:122,:);  % cut the outer boundary
+% features = computeImageFeatures(img);
+% features = ComputePositionColorFeatures(features);
+% features = NormalizeFeatures(features);
+% X = reshape(features, [], size(features, 3));
+% [idx,c] = KMeansClustering(X,13);
+% height = size(img, 1);
+% width = size(img, 2);
+% points = zeros(height, width, 2);
+% [points(:,:,1), points(:,:,2)] = meshgrid(1:width, height:-1:1);
+% points = reshape(points, [], 2);
+% VisualizeClusters2D(points, idx, c, figure);
+
 %% grabcut
 %grabcut('267_dive8_2014-09-29.jpg');
 
